9c6ca408cd40030f48fedaf9104630f4d55ed494
Anton Patsev <patsev.anton@gmail.com>
2026-09-13 18:51:36 +0600

    feat: вывод команды получения пароля администратора Coroot

--- coroot.tf
+++ coroot.tf
@@ -111,9 +111,9 @@ resource "local_file" "coroot_values" {
   ]
 }
 
-output "coroot_admin_secret_name" {
-  description = "Имя Kubernetes Secret с паролем администратора Coroot"
-  value       = kubernetes_secret.coroot_admin.metadata[0].name
+output "coroot_admin_password_command" {
+  description = "Команда kubectl для получения пароля администратора Coroot из секрета (логин admin)"
+  value       = "kubectl -n ${kubernetes_secret.coroot_admin.metadata[0].namespace} get secret ${kubernetes_secret.coroot_admin.metadata[0].name} -o jsonpath='{.data.admin-password}' | base64 -d"
 }
 
 output "coroot_admin_password" {
